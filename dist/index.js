c3286ac4dca70e56a90605bc0d0db0a7ae5aac02
Georgios Petrousis <georgios.petrousis@viaplay.com>
2019-07-15 17:42:36 +0200

    [#7] Add dist folder for the module

---
+++ dist/index.js
@@ -0,0 +1,3 @@
+/* eslint-disable import/prefer-default-export */
+import Typography from './typography';
+export { Typography };
---
+++ dist/typography/Styles.js
@@ -0,0 +1,14 @@
+import { css } from 'styled-components';
+const Font =
+/*#__PURE__*/
+css(["font-family:Raleway,'Open Sans',sans-serif;"]);
+const Normal =
+/*#__PURE__*/
+css(["", " font-weight:300;"], Font);
+const Bold =
+/*#__PURE__*/
+css(["", " font-weight:500;"], Font);
+const Muted =
+/*#__PURE__*/
+css(["opacity:0.6;"]);
+export { Normal, Bold, Muted };
