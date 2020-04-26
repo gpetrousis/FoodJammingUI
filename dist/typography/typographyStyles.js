edfc157696040b4aedbd2aa45b907b57158633d4
Georgios Petrousis <georgios.petrousis@nentgroup.com>
2020-04-26 19:14:50 +0200

    1.2.0

---
+++ dist/typography/typographyStyles.js
@@ -0,0 +1,22 @@
+'use strict';
+
+const Heading1 = {
+  element: 'h1',
+  fontSize: 32,
+  description: 'Large heading <h1> for brand titles or page titles (to be used rarely)'
+};
+const Heading2 = {
+  element: 'h2',
+  fontSize: 24,
+  description: 'Heading <h2> for page titles'
+};
+const Heading3 = {
+  element: 'h3',
+  fontSize: 16,
+  description: 'Small Heading <h3> for sections and post titles'
+};
+export default {
+  Heading1,
+  Heading2,
+  Heading3
+};
